2a4233fbf94d855f97e36157993e73c134169fc8
oPisiti <laurohrfranca@gmail.com>
2023-09-13 00:23:04 -0300

    First

---
+++ src/typing_speed.py
@@ -0,0 +1,176 @@
+from collections.abc import Iterable
+from enum import Enum
+from getch import getch
+from time import time, sleep
+
+import os
+import random
+import re
+import sys
+
+
+class Colors:
+    """
+    Nice colors :)
+    """
+
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAILRED   = '\033[91m'
+    ENDC      = '\033[0m'
+    BOLD      = '\033[1m'
+    UPONELINE = '\033[F'
+
+
+class Results(Enum):
+    """ Possible values for user input and corresponding color """
+
+    CORRECT       = Colors.OKGREEN
+    WRONG         = Colors.FAILRED
+    IN_PROGRESS   = Colors.BOLD
+    NOT_ATTEMPTED = Colors.ENDC
+
+
+class TypingSpeed():
+    """ Class variables """
+
+    WORDS_FILE_NAME = "words.txt"
+    WORDS_COUNT     = 10
+    CLEAR_COMMAND   = "cls" if os.name == "nt" else "clear"
+
+    def __init__(self, file_name: str = "words.txt"):
+
+        if file_name != TypingSpeed.WORDS_FILE_NAME: TypingSpeed.WORDS_FILE_NAME = file_name
+
+        # Parsing the words
+        try:
+            self.words_list = self.get_words(TypingSpeed.WORDS_FILE_NAME)
+        except FileNotFoundError as e:
+            sys.exit(f"File '{WORDS_FILE_NAME}' not found") 
+    
+        # Defining words
+        self.words = tuple(
+                      {"word": random.choice(self.words_list), 
+                       "status": Results.NOT_ATTEMPTED}
+                      for _ in range(TypingSpeed.WORDS_COUNT)
+                     )
+        self.letter_count = sum([len(w["word"]) for w in self.words])
+
+        self.score = 0
+
+
+    def get_input_letter(self) -> None:
+        """ Gets one letter at a time """
+
+        new_char = getch()   
+
+        # Correct char
+        if new_char == self.words[self.word_index]["word"][self.letter_index]:
+            self.words[self.word_index]["status"] = Results.IN_PROGRESS        
+            self.letter_index += 1
+
+            # Correct word
+            if self.letter_index >= len(self.words[self.word_index]["word"]):
+
+                self.words[self.word_index]["status"] = Results.CORRECT
+                self.word_index += 1
+                self.letter_index = 0
+                self.score += 1
+
+        # Wrong char
+        else:
+            self.words[self.word_index]["status"] = Results.WRONG
+            self.word_index += 1
+            self.letter_index = 0
+
+        # Updating the next word's color to in_progress
+        if self.word_index < len(self.words):
+            self.words[self.word_index]["status"] = Results.IN_PROGRESS
+            print(f'Word: {self.words[self.word_index]}')
+
+
+    @staticmethod
+    def get_words(file_name: str) -> tuple:
+        """ 
+        Returns a tuple containing all the words in a file.
+        File must be inside the same directory as this file's.
+        Raises FileNotFoundError if file is inaccessible.
+        """
+
+        file_path = os.path.join(os.path.dirname(__file__), file_name) 
+
+        with open(file_path) as f:
+            raw_data = f.read()
+
+        return tuple(re.findall("\w+", raw_data))
+
+
+    def render_frame(self) -> None:
+        """ Renders a single frame """
+
+        os.system(TypingSpeed.CLEAR_COMMAND)
+
+        print(f"Score:  {self.score}") 
+
+        word = self.words[self.word_index]['word']
+        print(f'Letter: {word[self.letter_index]}')
+        print(f"Word:   {word}")
+        
+        # Words already attempted
+        for i in range(self.word_index): 
+            print(self.words[i]["status"].value + self.words[i]["word"] + Colors.ENDC, end=" ")
+        
+        # Currently attempting word
+        current_status = Results.CORRECT
+        for i, letter in enumerate(self.words[self.word_index]["word"]):
+            if current_status == Results.IN_PROGRESS: current_status = Results.NOT_ATTEMPTED
+            if i == self.letter_index: current_status = Results.IN_PROGRESS
+            
+            print(current_status.value + letter + Colors.ENDC, end="")
+
+        print(end=" ")
+
+        # Others
+        for i in range(self.word_index + 1, len(self.words)):
+            print(self.words[i]["status"].value + self.words[i]["word"] + Colors.ENDC, end=" ")
+
+        print()
+
+
+    def render_gameover(self, dt: int) -> None:
+        """ 
+        Gameover splash screen.
+        dt is the time it took to complete execution.
+        """
+
+        os.system(TypingSpeed.CLEAR_COMMAND)
+
+        print(f'Final Score: {self.score}/{len(self.words)}')
+        print(f'Wpm: {60 * self.score / dt:.2f}')
+
+
+    def run(self) -> None:
+        """ Main game loop """
+
+        t0 = time()
+
+        # Game Loop
+        self.letter_index = 0
+        self.word_index   = 0
+
+        # Setting the status for the first word
+        self.words[self.word_index]["status"] = Results.IN_PROGRESS
+        
+        while self.word_index < len(self.words):
+            self.render_frame()
+            self.get_input_letter()
+
+        t1 = time()
+        self.render_gameover(t1 - t0)
+        input()
+
+
+
+if __name__ == '__main__':
+    game = TypingSpeed()
+    game.run()
